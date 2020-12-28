d8e40e8860a770d751383964f4d1abe40ba2abc6
tr0b <trobin3000600@gmail.com>
2020-12-28 16:27:34 -0600

    TinyCProject - Will add version 2.0 of Character Stream Changes: - Added version 2.0 of Character Stream in C

--- character_stream.c
+++ character_stream.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-/* Copy input to output; First version*/
+/* Copy input to output; Second version*/
 
 main(){
 
 	int c;
-	c=getchar();
-	while (c!=EOF) {
+	/* We can assign values to variables during a while call, and at the same time, compare its (while) condition in order to
+	 * break the loop. */
+	while ((c=getchar())!=EOF) {
 
 		putchar(c);
-		c=getchar();
 	}
 }
